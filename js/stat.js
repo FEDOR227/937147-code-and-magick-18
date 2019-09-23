b35b2a6186e480784eacac0d2e5789808eeea8f9
FEDOR227 <FEDOR>
2019-09-23 21:48:59 +0300

    Код и Магия

--- js/stat.js
+++ js/stat.js
@@ -1,82 +1,53 @@
-
 'use strict';
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 110;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BLACK = '#000';
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
 window.renderStatistics = function (ctx, names, times) {
-  var dataCloud = {
-    startX: 100,
-    startY: 10,
-    widthRect: 420,
-    heightRect: 270,
-    lenghtShadow: 10,
-    margin: 40,
 
-    colorRect: ['rgba(0, 0, 0, 0.7)', 'rgb(256, 256, 256)'],
-    text: ['Ура вы победили!', 'Список результатов: ']
-  };
-  drawRect(dataCloud.startX + dataCloud.lenghtShadow, dataCloud.startY + dataCloud.lenghtShadow, dataCloud.widthRect, dataCloud.heightRect, dataCloud.colorRect[0]);
-  drawRect(dataCloud.startX, dataCloud.startY, dataCloud.widthRect, dataCloud.heightRect, dataCloud.colorRect[1]);
-  writeText(dataCloud.text);
-  drawHistogram(times, names);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7 )');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  function drawRect(axisX, axisY, width, height, fillColor) {
-    ctx.fillStyle = fillColor;
-    ctx.fillRect(axisX, axisY, width, height);
-  }
+  ctx.fillStyle = BLACK;
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', 120, 20);
+  ctx.fillText('Список результатов:', 120, 40);
 
-  function writeText(textArray) {
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
+  var maxTime = getMaxElement(times);
+  for (var i = 0; i < names.length; i++) {
+    var STAT_POSITION_X = (CLOUD_X + GAP) + ((i + 1) * 70);
+    var STAT_HEIGHT = -times[i] / maxTime * BAR_HEIGHT;
 
-    for (var i = 0; i < textArray.length; i++) {
-      ctx.fillText(textArray[i], dataCloud.startX + dataCloud.margin, dataCloud.startY + (i + 1) * 25);
-    }
-  }
+    ctx.fillStyle = BLACK;
+    ctx.fillText(names[i], (CLOUD_X + GAP) + ((i + 1) * 70), CLOUD_HEIGHT - GAP * 2);
+    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random() + ')';
+    ctx.fillRect(STAT_POSITION_X, CLOUD_HEIGHT - GAP * 3, BAR_WIDTH, STAT_HEIGHT);
 
-  function drawHistogram(arrayTimes, arrayNames) {
 
-    var dataHistogram = {
-      barWidth: 40,
-      indent: 90,
-      indentName: 20,
-      indentTime: 10,
-      histogramHeight: 150,
-      paddingTop: 60,
-    };
-
-    var step = dataHistogram.histogramHeight / (getMaxValue(times) - 0);
-    var initialX = dataCloud.startX + dataCloud.margin;
-    var initialY = dataCloud.startY + dataHistogram.histogramHeight + dataHistogram.indentName + dataHistogram.indentTime + dataHistogram.paddingTop;
-
-    for (var i = 0; i < arrayTimes.length; i++) {
-      dataHistogram.barHeight = arrayTimes[i] * step;
-      var getY = initialY - arrayTimes[i] * step;
-      var getX = initialX + dataHistogram.indent * i;
-
-      ctx.fillStyle = fillBarColor(names[i]);
-      ctx.fillRect(getX, getY, dataHistogram.barWidth, dataHistogram.barHeight);
-
-      ctx.fillText(arrayNames[i], getX, initialY + dataHistogram.indentName);
-      ctx.fillText(arrayTimes[i].toFixed(0), getX, getY - dataHistogram.indentTime);
-    }
-  }
-
-  function getMaxValue(array) {
-    var max = -1;
-    for (var i = 0; i < array.length; i++) {
-      var value = array[i];
-      if (value > max) {
-        max = value;
-      }
-    }
-    return max;
-  }
-
-  function fillBarColor(namePlayer) {
-    var randomOpacity = Math.random().toFixed(2);
-    if (namePlayer === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
-    }
+    ctx.fillStyle = BLACK;
+    ctx.fillText(Math.round(times[i]), STAT_POSITION_X, CLOUD_HEIGHT - GAP * 5 + STAT_HEIGHT);
   }
 };
