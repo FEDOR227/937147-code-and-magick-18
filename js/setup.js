7e66db21c7a8f1782ab137523af3f2743f6dbdec
kolty <koltyrinfedor32@gmail.com>
2019-10-07 22:07:38 +0300

    937147-code-and-magick-18

---
+++ js/setup.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор',  'Юлия',
+    'Люпита',  'Вашингтон'];
+
+var SUR_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко',
+  'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+
+var setup = document.fragment.querySelector('.setup');
+setup.classList.remove('hidden');
+
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
+
+var similarListElement = setupSimilar.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
+function getRandomElement(arr) {
+  return arr[Math.floor(arr.length * Math.random())];
+}
+
+
+function createWizards(num) {
+  var wizards = [];
+
+  for (var i = 0; i < num; i++) {
+    wizards[i] = {
+      name: getRandomElement(NAMES) + ' ' + getRandomElement(SUR_NAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    };
+
+  }
+  return wizards;
+}
+
+function renderWizard(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+}
+
+function renderAllWizards(wizards) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+
+  return fragment;
+}
+
+similarListElement.appendChild(renderAllWizards(createWizards(4)));
